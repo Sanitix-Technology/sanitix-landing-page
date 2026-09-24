3ec7e50cf655efc863f42d7123c271a0fa91c9a1
Trayam <121534704+trayamm18@users.noreply.github.com>
2026-09-24 19:23:47 +0530

    Update registered office address to Sector-37C Gurgaon

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -139,7 +139,8 @@ export const Footer: React.FC<FooterProps> = ({ onOpenBooking: _onOpenBooking, o
                   <MapPin size={15} color="#10B981" style={{ marginTop: '3px', flexShrink: 0 }} />
                   <span style={{ fontSize: '13px', color: '#9CA3AF', lineHeight: 1.45 }}>
                     Sanitix Technologies Private Limited<br />
-                    Gurugram, Haryana, India
+                    D-702, Floor-7F, Tower-3, RMG Residency<br />
+                    Sector-37C, Gurgaon, Haryana – 122001
                   </span>
                 </div>
               </div>
@@ -633,7 +634,7 @@ export const Footer: React.FC<FooterProps> = ({ onOpenBooking: _onOpenBooking, o
                       <strong style={{ color: '#10B981' }}>Sanitix Technologies Private Limited (A Pairow Group Company)</strong><br />
                       Email: <a href="mailto:support@sanitix.in" style={{ color: '#10B981', textDecoration: 'none' }}>support@sanitix.in</a><br />
                       Helpline: <a href="tel:+912269710156" style={{ color: '#10B981', textDecoration: 'none' }}>+91 22697 10156</a><br />
-                      Corporate Office: Gurugram, Haryana, India
+                      Corporate Office: D-702, Floor-7F, Tower-3, RMG Residency, Sector-37C, Gurgaon, Haryana – 122001, India
                     </div>
                   </div>
                 </div>
@@ -736,7 +737,7 @@ export const Footer: React.FC<FooterProps> = ({ onOpenBooking: _onOpenBooking, o
                       <strong style={{ color: '#10B981' }}>Sanitix Technologies Private Limited</strong><br />
                       Email: <a href="mailto:support@sanitix.in" style={{ color: '#10B981', textDecoration: 'none' }}>support@sanitix.in</a><br />
                       Phone: <a href="tel:+912269710156" style={{ color: '#10B981', textDecoration: 'none' }}>+91 22697 10156</a><br />
-                      Corporate Office: Gurugram, Haryana, India
+                      Corporate Office: D-702, Floor-7F, Tower-3, RMG Residency, Sector-37C, Gurgaon, Haryana – 122001, India
                     </div>
                   </div>
                 </div>
